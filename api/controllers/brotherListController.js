d5c3e926ad535d55588143b9d0a2d366c82bba3c
Jonathan <samsonjj@vcu.edu>
2018-04-19 23:08:26 -0400

    changed database access method, should persist even when database goes down

--- api/controllers/brotherListController.js
+++ api/controllers/brotherListController.js
@@ -6,15 +6,14 @@ const dbHostName = 'kgot-database.cuitsbqfftu2.us-east-2.rds.amazonaws.com';
 const dbPort = 3306;
 const dbUsername = "kgotwebsite";
 const dbPassword = "kgotwebsite";
-const dbName = "kgotdatabase";
+const dbDatabase = "kgotdatabase";
 
-var con = mysql.createConnection({
+/*var con = mysql.createConnection({
 	host: dbHostName,
 	port: dbPort,
 	user: dbUsername,
 	password: dbPassword,
 	database: dbName
-})
 
 con.connect(function(err) {
 	if(err) {
@@ -22,20 +21,60 @@ con.connect(function(err) {
 		return;
 	}
 	console.log('connected');
+});*/
+
+var pool  = mysql.createPool({
+  host     	: dbHostName,
+  port 		: dbPort,
+  user     	: dbUsername,
+  password 	: dbPassword,
+  database 	: dbDatabase
 });
 
 exports.list_active_brothers = function(req, res) {
-	con.query("select * from Brothers where active = 1", function(err, result) {
+	console.log("active called");
+	pool.getConnection(function(err, connection) {
+    if(err) { 
+      console.log(err);  
+      return; 
+    }
+    var sql = "select * from Brothers where active = 1";
+    connection.query(sql, [], function(err, result) {
+      connection.release(); // always put connection back in pool after last query
+      if(err) { 
+        console.log(err);  
+        return; 
+      }
+      res.send(result);
+    });
+  });
+	/*con.query("select * from Brothers where active = 1", function(err, result) {
 		if (err) throw err;
 		res.send(result);
-	});
+	});*/
 }
 
 exports.list_all_brothers = function(req, res) {
+	/*
 	con.query("select * from Brothers", function(err, result) {
 		if (err) throw err;
 		res.send(result);
-	});
+	});*/
+	pool.getConnection(function(err, connection) {
+    if(err) { 
+      console.log(err);  
+      return; 
+    }
+    var sql = "select * from Brothers";
+    connection.query(sql, [], function(err, result) {
+      connection.release(); // always put connection back in pool after last query
+      if(err) { 
+        console.log(err);  
+        return; 
+      }
+      res.send(result);
+    });
+  });
 }
 
 exports.create_a_brother = function(req, res) {
@@ -43,10 +82,21 @@ exports.create_a_brother = function(req, res) {
 }
 
 exports.read_a_brother = function(req, res) {
-	con.query("select * from Brothers where roll = " + req.params.rollNumber, function(err, result) {
-		if (err) throw err;
-		res.send(result);
-	});
+	pool.getConnection(function(err, connection) {
+    if(err) { 
+      console.log(err);  
+      return; 
+    }
+    var sql = "select * from Brothers where roll = " + req.params.rollNumber;
+    connection.query(sql, [], function(err, result) {
+      connection.release(); // always put connection back in pool after last query
+      if(err) { 
+        console.log(err);  
+        return; 
+      }
+      res.send(result);
+    });
+  });
 }
 
 exports.update_a_brother = function(req, res) {
@@ -56,3 +106,22 @@ exports.update_a_brother = function(req, res) {
 exports.delete_a_brother = function(req, res) {
 	res.send('delete_a_brother');
 }
+
+/*
+queryDatabase = function(sql) {
+	pool.getConnection(function(err, connection) {
+    if(err) { 
+      console.log(err); 
+      callback(true); 
+      return; 
+    }
+    var sql = "select * from Brothers where active = 1";
+    connection.query(sql, [], function(err, result) {
+      connection.release(); // always put connection back in pool after last query
+      if(err) { 
+        console.log(err);  
+        return; 
+      }
+      res.send(result);
+    });
+}*/
--- api/routes/brotherListRoutes.js
+++ api/routes/brotherListRoutes.js
@@ -7,11 +7,11 @@ module.exports = function(app) {
     .get(brotherList.list_all_brothers)
     .post(brotherList.create_a_brother);
 
-  app.route('/brothers/:rollNumber')
+  app.route('/brothers/roll-:rollNumber')
     .get(brotherList.read_a_brother)
     .put(brotherList.update_a_brother)
     .delete(brotherList.delete_a_brother);
 
-  app.route('/active')
+  app.route('/brothers/active')
   	.get(brotherList.list_active_brothers);
 };
